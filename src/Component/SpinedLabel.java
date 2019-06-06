e804d1fd8493ac9872fc54707e1758373097a4ee
extJo <josungyun6518@gmail.com>
2019-06-06 14:54:57 +0900

    Change SpinedLabel
    change convention to fit java convention

--- src/Component/SpinedLabel.java
+++ src/Component/SpinedLabel.java
@@ -7,15 +7,15 @@ import javax.swing.JLabel;
 
 import src.Model.Voice;
 
-public class SpinedLabel extends JLabel{
-    public Voice voice; //stored for possible future uses
-    
-    public SpinedLabel(Voice voice){
-        super();
-        this.voice=voice;
-        
-        this.setOpaque(true);
-        this.setBackground(voice.getColor());
-        this.setBorder(BorderFactory.createLineBorder(Color.black));
-    }
+public class SpinedLabel extends JLabel {
+	public Voice voice; // stored for possible future uses
+
+	public SpinedLabel(Voice voice) {
+		super();
+		this.voice = voice;
+
+		this.setOpaque(true);
+		this.setBackground(voice.getColor());
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
+	}
 }
