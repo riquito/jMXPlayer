959c6b7d38531266dce9c9f732d25dd463a46ca2
extJo <josungyun6518@gmail.com>
2019-06-10 18:58:45 +0900

    remove unused file

--- src/Controller/CanvasResizedEvent.java
+++
@@ -1,8 +0,0 @@
-package src.Controller;
-
-//Declare the event. It must extend EventObject.
-class CanvasResizedEvent extends java.util.EventObject {
-	public CanvasResizedEvent(Object source) {
-	    super(source);
-	}
-}
