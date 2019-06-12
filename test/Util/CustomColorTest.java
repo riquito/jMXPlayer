32c8fbcaa673c7752b42de1d9e007a13a99d71ca
extJo <josungyun6518@gmail.com>
2019-06-12 14:58:41 +0900

    Purpose: Get Next Color 30 Times Input: Expected:
    return SUCCESS
    color list from getNextColor instanceof List of Color

--- test/Util/CustomColorTest.java
+++ test/Util/CustomColorTest.java
@@ -3,6 +3,9 @@ package test.Util;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
 import src.Util.CustomColor;
 
 public class CustomColorTest {
@@ -18,6 +21,23 @@ public class CustomColorTest {
 	@Test
 	public void getNextColorTest() {
 		assertTrue(CustomColor.getNextColor() instanceof Color);
+	}
+	
+	/**
+	* Purpose: Get Next Color 30 Times 
+	* Input: 
+	* Expected:
+	*
+	*	return SUCCESS
+	*	color list from getNextColor instanceof List of Color
+	*/
+	@Test
+	public void getNextColorListTest() {
+		List<Color> colorList = new ArrayList<>();
+		for (int i = 0; i < 20; i++) {
+			colorList.add(CustomColor.getNextColor());
+		}
+		assertNotNull(colorList.size());
 	}
 	
 	/**
