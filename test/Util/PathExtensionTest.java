50f8d43c8b28bc34df79f412c81e239915c55eee
extJo <josungyun6518@gmail.com>
2019-06-12 23:30:59 +0900

    Purpose: Path Join Test with weird base path Input: "/user/bin\\", "images/flowers/rose1.jpeg" Expected: "/user/bin/images/flowers/rose1.jpeg"
    return SUCCESS
    
    "/user/bin\\" + "images/flowers/rose1.jpeg"
    "/user/bin/images/flowers/rose1.jpeg"

--- test/Util/PathExtensionTest.java
+++ test/Util/PathExtensionTest.java
@@ -38,4 +38,21 @@ public class PathExtensionTest {
 		assertEquals(PathExtension.join("/user/bin:", "images/flowers/rose1.jpeg"),
 				"/user/bin/images/flowers/rose1.jpeg");
 	}
+	
+	/**
+	 * Purpose: Path Join Test with weird base path
+	 * Input: "/user/bin\\", "images/flowers/rose1.jpeg"
+	 * Expected: "/user/bin/images/flowers/rose1.jpeg"
+	 *
+	 * return SUCCESS
+	 * 
+	 * "/user/bin\\" + "images/flowers/rose1.jpeg"
+	 * "/user/bin/images/flowers/rose1.jpeg"
+	 */
+	@Test
+	public void pathJoinTestWithWeirdBasePath2() {
+		assertEquals(PathExtension.join("/user/bin\\", "images/flowers/rose1.jpeg"),
+				"/user/bin/images/flowers/rose1.jpeg");
+	}
+	
 }
