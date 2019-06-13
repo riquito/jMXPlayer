a89684040e099ae70aa1e4077d53afd835a477e7
extJo <josungyun6518@gmail.com>
2019-06-13 13:58:36 +0900

    Purpose: create Simple Toggle Button Input: Expected: equal to JToggleButton with empty text, no icon, doesn't selected, doesn't enable, no actionListener
    return SUCCESS
    
    equal

--- test/Component/ToggleButtonTest.java
+++ test/Component/ToggleButtonTest.java
@@ -1,14 +1,25 @@
 package test.Component;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import javax.swing.JToggleButton;
+
+import src.Component.ToggleButton;
 
 public class ToggleButtonTest {
-
+	/**
+	 * Purpose: create Simple Toggle Button  
+	 * Input: 
+	 * Expected: equal to JToggleButton with empty text, no icon, 
+	 * doesn't selected, doesn't enable, no actionListener
+	 *
+	 * return SUCCESS
+	 * 
+	 * equal 
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateSimpleToggleButton() {
+		assertTrue(new ToggleButton.Builder().build() instanceof JToggleButton);
 	}
 
 }
