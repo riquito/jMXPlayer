ca0a606319b2689585acf8d9faef79c401f154af
extJo <josungyun6518@gmail.com>
2019-06-13 18:44:45 +0900

    Purpose: check getSpine2time() is instance of Hashtable Input: Expected: True
    return SUCCESS
    
    check getSpine2time() is instance of Hashtable
    True

---
+++ test/Model/AudioClipTest.java
@@ -0,0 +1,14 @@
+package test.Model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AudioClipTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
