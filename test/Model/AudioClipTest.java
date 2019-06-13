1998087ab474e126a542985b6ac88ca551a72355
extJo <josungyun6518@gmail.com>
2019-06-13 18:45:24 +0900

    Purpose: add (spine = "abcd" : time = "30") key value Input: "abcd", 30 Expected: NotNull
    return SUCCESS
    
    add (spine = "abcd" : time = "30") key value
    NotNull

--- test/Model/AudioClipTest.java
+++ test/Model/AudioClipTest.java
@@ -1,14 +1,52 @@
 package test.Model;
 
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
-public class AudioClipTest {
+import java.util.Hashtable;
+import src.Model.AudioClip;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+public class AudioClipTest {
+	AudioClip audioClip;
+	
+	@Before
+	public void setUp() {
+		audioClip = new AudioClip();
 	}
+
+	/**
+	 * Purpose: check getSpine2time() is instance of Hashtable 
+	 * Input:
+	 * Expected: True
+	 *
+	 * return SUCCESS
+	 * 
+	 * check getSpine2time() is instance of Hashtable
+	 * True 
+	 */
+	@Test
+	public void testGetSpineTime() {
+		assertTrue(audioClip.getSpine2time() instanceof Hashtable);
+	}
+
+	/**
+	 * Purpose: add (spine = "abcd" : time = "30") key value  
+	 * Input: "abcd", 30
+	 * Expected: NotNull
+	 *
+	 * return SUCCESS
+	 * 
+	 * add (spine = "abcd" : time = "30") key value
+	 * NotNull 
+	 */
+	@Test
+	public void testAddSpineTime() {
+		String spine = "abcd";
+		int time = 30;
+		audioClip.addSpineTime(spine, time);
+		assertNotNull(audioClip.getSpine2time().containsKey(spine));
+	}
+
 
 }
