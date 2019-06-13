a935adf9cf9556a04755a4497de9e9421f63589e
extJo <josungyun6518@gmail.com>
2019-06-13 14:03:38 +0900

    Purpose: create Toggle Button with Icon Input: Icon = ResourceLoader.getResourceUrl("http://jjalbang.today/jj237.jpg", "") Expected: equal to JToggleButton with empty text, "ResourceLoader.getResourceUrl("http://jjalbang.today/jj237.jpg", "")" icon, doesn't selected, doesn't enable, no actionListener
    return SUCCESS
    
    equal

--- test/Component/ToggleButtonTest.java
+++ test/Component/ToggleButtonTest.java
@@ -1,10 +1,17 @@
 package test.Component;
 
 import static org.junit.Assert.*;
+
+import java.net.MalformedURLException;
+
 import org.junit.Test;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JToggleButton;
 
 import src.Component.ToggleButton;
+import src.Util.ResourceLoader;
 
 public class ToggleButtonTest {
 	/**
@@ -37,6 +44,24 @@ public class ToggleButtonTest {
 		String text = "hello";
 		JToggleButton toggleButton = new ToggleButton.Builder().text(text).build(); 
 		assertEquals(toggleButton.getText(), text);
+	}
+	
+	/**
+	 * Purpose: create Toggle Button with Icon  
+	 * Input: Icon = ResourceLoader.getResourceUrl("http://jjalbang.today/jj237.jpg", "")
+	 * Expected: equal to JToggleButton with empty text, 
+	 * "ResourceLoader.getResourceUrl("http://jjalbang.today/jj237.jpg", "")" icon, 
+	 * doesn't selected, doesn't enable, no actionListener
+	 *
+	 * return SUCCESS
+	 * 
+	 * equal 
+	 */
+	@Test
+	public void testCreateWithIconToggleButton() throws MalformedURLException {
+		Icon icon = new ImageIcon(ResourceLoader.getResourceUrl("http://jjalbang.today/jj237.jpg", ""));
+		JToggleButton toggleButton = new ToggleButton.Builder().icon(icon).build(); 
+		assertEquals(toggleButton.getIcon(), icon);
 	}
 
 }
