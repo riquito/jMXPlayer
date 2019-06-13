cb7991fef1b9692c06d549828a9a9248f09c6258
wnstner92 <38302822+wnstner92@users.noreply.github.com>
2019-06-13 20:25:48 +0900

    Purpose: add CanvasResizeEventListener
    /**
    	 * Purpose: add CanvasResizeEventListener
    	 * Input: 
    	 * Expected: not Null
    	 * 		return SUCCESS
    	 * 		imageCanvas -> not null
    	 * 		
    	 *
    	 */

--- test/Component/ImageCanvasTest.java
+++ test/Component/ImageCanvasTest.java
@@ -1,14 +1,21 @@
 package test.Component;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+
+import javax.swing.JToggleButton;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import src.Component.ImageCanvas;
+import src.Component.ToggleButton;
+import src.Controller.CanvasResizedListener;
 
 public class ImageCanvasTest {
 	
@@ -44,5 +51,21 @@ public class ImageCanvasTest {
 		imageCanvas.setImage(image);
 		assertNotNull(imageCanvas);
 	}
-
+	
+	/**
+	 * Purpose: add CanvasResizeEventListener
+	 * Input: 
+	 * Expected: not Null
+	 * 		return SUCCESS
+	 * 		imageCanvas -> not null
+	 * 		
+	 *
+	 */
+	@Test
+	public void testaddCanvasResizeEventListener() {
+		CanvasResizedListener listener = null;
+		imageCanvas.addCanvasResizeEventListener(listener);
+		assertNotNull(imageCanvas);
+	}
+	
 }
